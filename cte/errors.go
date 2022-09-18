bf91b1fdc4e0a2e78fb6c3be475d2542de645800
James Tran <4ccount.manager@gmail.com>
2022-09-18 14:54:05 +0800

    add error codes

--- cte/errors.go
+++ cte/errors.go
@@ -27,28 +27,28 @@ var (
 	//
 	// Note: If the ending plan is nested inside another plan, the outer plan will still continue
 	// as usual.
-	ErrPlanExecutionEndingEarly = errors.New("plan execution ending early")
+	ErrPlanExecutionEndingEarly = errors.New("CTE-001: plan execution ending early")
 	// ErrRootPlanExecutionEndingEarly can be thrown actively by clients to end plan execution
 	// early. For example, a value was retrieved from cache and thus, there's no point executing
 	// the algo to calculate this value anymore. The engine will swallow this error, end execution
 	// and then return a nil error to clients.
 	//
 	// Note: If the ending plan is nested inside another plan, the outer plan will also end.
-	ErrRootPlanExecutionEndingEarly = errors.New("plan execution ending early from root")
+	ErrRootPlanExecutionEndingEarly = errors.New("CTE-0002: plan execution ending early from root")
 
-	ErrPlanMustUsePointerReceiver = makeFormatErr("%v is using value receiver, all plans must be implemented using pointer receiver")
-	ErrPlanNotAnalyzed            = makeFormatErr("%v has not been analyzed yet, call AnalyzePlan on it first")
-	ErrNestedPlanCannotBePointer  = makeFormatErr("%v has a nested plan called %v that is a pointer")
+	ErrPlanMustUsePointerReceiver = makeFormatErr("CTE-0003: %v is using value receiver, all plans must be implemented using pointer receiver")
+	ErrPlanNotAnalyzed            = makeFormatErr("CTE-0004: %v has not been analyzed yet, call AnalyzePlan on it first")
+	ErrNestedPlanCannotBePointer  = makeFormatErr("CTE-0005: %v has a nested plan called %v that is a pointer")
 
-	ErrPlanNotMeetingInoutRequirements            = makeFormatErr("%v does not implement the required in-out interface %v, problem found: %v. Component having problem: %v")
+	ErrPlanNotMeetingInoutRequirements            = makeFormatErr("CTE-0006: %v does not implement the required in-out interface %v, problem found: %v. Component having problem: %v")
 	ErrPlanMissingMethod                          = makeFormatErr("missing method: [%v]")
 	ErrPlanHavingAmbiguousMethods                 = makeFormatErr("required method: [%v], found ambiguous methods: [%v], components carrying the ambiguous methods: [%v]")
 	ErrPlanHavingSameMethodRegisteredMoreThanOnce = makeFormatErr("required method provided more than once by the same computer key: [%v], computer key locations: [%v]")
 	ErrPlanHavingMethodButSignatureMismatched     = makeFormatErr("required method: [%v], found method with mismatched signature: [%v]")
 
-	ErrInvalidComputerType = makeFormatErr("%v is not a computer")
-	ErrMetadataMissing     = makeFormatErr("metadata is missing for %v, it must implement the MetadataProvider interface")
-	ErrNilMetadata         = makeFormatErr("metadata is nil for %v")
-	ErrComputerMetaMissing = makeFormatErr("computer meta is missing in %v")
-	ErrInoutMetaMissing    = makeFormatErr("inout meta is missing in %v")
+	ErrInvalidComputerType = makeFormatErr("CTE-0007: %v is not a computer")
+	ErrMetadataMissing     = makeFormatErr("CTE-0008: metadata is missing for %v, it must implement the MetadataProvider interface")
+	ErrNilMetadata         = makeFormatErr("CTE-0009: metadata is nil for %v")
+	ErrComputerMetaMissing = makeFormatErr("CTE-0010: computer meta is missing in %v")
+	ErrInoutMetaMissing    = makeFormatErr("CTE-0011: inout meta is missing in %v")
 )
